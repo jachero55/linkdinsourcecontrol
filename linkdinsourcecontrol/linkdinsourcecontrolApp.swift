bf8713054c3548a98f44cae235d37d1c6489a4ad
John Achero <jkachero@student.fullsail.edu>
2022-01-19 13:15:13 -0600

    Initial Commit

---
+++ linkdinsourcecontrol/linkdinsourcecontrolApp.swift
@@ -0,0 +1,17 @@
+//
+//  linkdinsourcecontrolApp.swift
+//  linkdinsourcecontrol
+//
+//  Created by John Achero on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct linkdinsourcecontrolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
